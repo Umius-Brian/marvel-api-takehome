b40cc7c47229cf398774c714a2b85e3d820e9ecc
Brian Bui <59684534+Umius-Brian@users.noreply.github.com>
2020-07-19 00:23:01 -0700

    Modify config file for API key data retrieval

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,35 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+const dotenv = require("dotenv")
+
+const setupEnvVariables = () => {
+  dotenv.config({
+    path: `.env.${process.env.NODE_ENV}`
+  })
+}
+
+setupEnvVariables()
+
+const {
+  env: { GATSBY_PUBLIC_KEY, GATSBY_PRIVATE_KEY }
+} = process
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: "gatsby-source-marvel",
+      options: {
+        publicKey: GATSBY_PUBLIC_KEY,
+        privateKey: GATSBY_PRIVATE_KEY,
+        queries: [
+          {
+            entity: "characters",
+            entityFilters: {
+              name: "Iron Man",
+            }
+          }
+        ]
+      }
+    }
+  ],
 }
--- src/templates/Character/index.js
+++ src/templates/Character/index.js
@@ -1,2 +1,8 @@
 import React from "react"
 
+export class Character extends Component {
+  render() {
+    const { file, charactersNode} = this.props.data;
+
+  }
+}
