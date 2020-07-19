2b4b845cd75789a843f7941db54ae95dbeb96089
Brian Bui <59684534+Umius-Brian@users.noreply.github.com>
2020-07-19 01:07:43 -0700

    Create GraphQL query and Layout component

---
+++ src/components/Layout/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { string } from "prop-types"
+
+export class Layout extends Component {
+  static propTypes = {
+    title: string.isRequired,
+  }
+
+  render() {
+    const { children, title } = this.props;
+    return (
+      <div>
+        <Helmet>
+          <title>{title}</title>
+        </Helmet>
+        {children}
+      </div>
+    )  
+  }
+}
+
+export default Layout;
--- src/templates/Character/index.js
+++ src/templates/Character/index.js
@@ -1,8 +1,44 @@
-import React from "react"
+import React from "react";
+import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Header from "./Header"
+import Details from "./Details"
+import Comics from "./Comics"
+
 
 export class Character extends Component {
   render() {
-    const { file, charactersNode} = this.props.data;
-
+    const { charactersNode, file } = this.props.data;
+    const { name, description, thumbnail } = charactersNode;
+    return (
+      <Layout title={name}>
+        <Header thumbnail={file} description={description} />
+        <Details thumbnail={thumbnail} />
+        <Comics />
+      </Layout>
+    )
   }
-}
+};
+
+export const query = graphql`
+  {
+    charactersNode {
+      description
+      name
+      id
+      thumbnail {
+        path
+        extension
+      }
+    };
+    file(name: { eq: "character" }) {
+      childImageSharp {
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  };
+`
+
+export default Character;
